37d34ee0d68d27650147b716413f0c786bcda4b3
Santhosh S <santhoshcsoffl@gmail.com>
2026-03-28 11:00:43 +0530

    feat: set baseURL to '/' in ApiClient axios instance

--- frontend/src/lib/api-client.ts
+++ frontend/src/lib/api-client.ts
@@ -7,7 +7,7 @@ class ApiClient {
 
   constructor() {
     this.client = axios.create({
-      baseURL: '',
+      baseURL: '/',
       timeout: 60000, // 60 seconds
       headers: {
         'Content-Type': 'application/json',
